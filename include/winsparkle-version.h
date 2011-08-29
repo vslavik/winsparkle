7bffa3d1a35daee1b416384ed81de701df80c88b
Vaclav Slavik <vslavik@gmail.com>
2011-08-29 11:35:36 +0200

    Increase version to 0.4, update release notes.

--- include/winsparkle-version.h
+++ include/winsparkle-version.h
@@ -31,7 +31,7 @@
  *--------------------------------------------------------------------------*/
 
 #define WIN_SPARKLE_VERSION_MAJOR   0
-#define WIN_SPARKLE_VERSION_MINOR   3
+#define WIN_SPARKLE_VERSION_MINOR   4
 #define WIN_SPARKLE_VERSION_MICRO   0
 
 /**
