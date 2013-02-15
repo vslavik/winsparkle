f77c7852fb642e88059b1b4191f3d7634bdd4ab6
Vaclav Slavik <vslavik@gmail.com>
2013-02-15 16:20:26 +0100

    Add x64 build configurations.
    Fixes #12.

--- examples/psdk/app_psdk.c
+++ examples/psdk/app_psdk.c
@@ -29,7 +29,7 @@
 
 /* force use of Common Controls 6 */
 #ifdef _MSC_VER
-#pragma comment(linker, "\"/manifestdependency:type='Win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='X86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#pragma comment(linker, "\"/manifestdependency:type='Win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #endif
 
 
