b8a16abed04761ec4db04c70195e672ccf00f4b0
Václav Slavík <vaclav@slavik.io>
2015-06-08 10:56:35 +0200

    Update wxWidgets

--- 3rdparty/wxWidgets_setup_h/wx/setup.h
+++ 3rdparty/wxWidgets_setup_h/wx/setup.h
@@ -4,7 +4,6 @@
 // Author:      Julian Smart
 // Modified by:
 // Created:     01/02/97
-// RCS-ID:      $Id: setup0.h 62704 2009-11-24 00:01:15Z VZ $
 // Copyright:   (c) Julian Smart
 // Licence:     wxWindows licence
 /////////////////////////////////////////////////////////////////////////////
