vslavik/winsparkle

commit da4bd8ccf3a9a4884e48546c54975152db755b2b

Handle connection closed events during download (#114)
There are cases where INTERNET_STATUS_CONNECTION_CLOSED events could
be triggered during a download transfer (due to network connectivity
issues).

Previously, WinSparkle would erroneously report the download as
successfully completed due to the fact that InternetReadFileEx would
return TRUE with 'dwBufferLength' set to zero after the connection
closed event.

This commit aims to gracefully handle these events (by throwing an
exception) via doing the following:
- When an INTERNET_STATUS_CONNECTION_CLOSED event is triggered,
immediately close the connection handle so that future API calls will
return failure (e.g. ERROR_INVALID_HANDLE).
- If the download has completed (indicated by 'dwBufferLength' being
zero), check that the connection handle is still valid. This detects
the case when the INTERNET_STATUS_CONNECTION_CLOSED event is received
and the connection handle is closed during a call to
InternetReadFileEx.

src/download.cpp
@@ -51,18 +51,24 @@ struct InetHandle
 
     ~InetHandle()
     {
-        if (m_handle)
-        {
-            if (m_callback)
-                InternetSetStatusCallback(m_handle, NULL);
-            InternetCloseHandle(m_handle);
-        }
+        Close();
     }
 
     void SetStatusCallback(INTERNET_STATUS_CALLBACK callback)
     {
         m_callback = callback;
         InternetSetStatusCallback(m_handle, m_callback);
+    }
+
+    void Close()
+    {
+        if (m_handle)
+        {
+            if (m_callback)
+                InternetSetStatusCallback(m_handle, NULL);
+            InternetCloseHandle(m_handle);
+            m_handle = NULL;
+        }
     }
 
     operator HINTERNET() const { return m_handle; }
@@ -141,6 +147,10 @@ void CALLBACK DownloadInternetStatusCallback(_In_ HINTERNET hInternet,
 
         case INTERNET_STATUS_REQUEST_COMPLETE:
             context->eventRequestComplete.Signal();
+            break;
+
+        case INTERNET_STATUS_CONNECTION_CLOSED:
+            context->conn->Close();
             break;
     }
 }
@@ -297,7 +307,14 @@ void DownloadFile(const std::string& url, IDownloadSink *sink, Thread *onThread,
         }
 
         if (ibuf.dwBufferLength == 0)
-            break; // all of the file was downloaded
+        {
+            // This check is required in case the INTERNET_STATUS_CONNECTION_CLOSED event was 
+            // received (and the handle was closed) during the call to InternetReadFileEx()
+            if (!conn)
+                throw Win32Exception();
+            else
+                break; // all of the file was downloaded
+        }
 
         sink->Add(ibuf.lpvBuffer, ibuf.dwBufferLength);
     }

commit 78f865461f85ba72b8b69e637927e280856ad122

Check for download errors properly
Replace the hodgepodge of unreliable checks for download failures
introduced in #114 and then #139 with proper check for errors: the
dwError field in INTERNET_STATUS_REQUEST_COMPLETE. This makes the code
simpler _and_ more reliable.

Fixes #147.

src/download.cpp
@@ -135,9 +135,9 @@ std::wstring GetURLFileName(const char *url)
 
 struct DownloadCallbackContext
 {
-    DownloadCallbackContext(InetHandle *conn_) : conn(conn_), wasClosed(false) {}
+    DownloadCallbackContext(InetHandle *conn_) : conn(conn_), lastError(ERROR_SUCCESS) {}
     InetHandle *conn;
-    bool wasClosed;
+    DWORD lastError;
     Event eventRequestComplete;
 };
 
@@ -157,27 +157,8 @@ void CALLBACK DownloadInternetStatusCallback(_In_ HINTERNET hInternet,
             break;
 
         case INTERNET_STATUS_REQUEST_COMPLETE:
+            context->lastError = res->dwError;
             context->eventRequestComplete.Signal();
-            break;
-
-        case INTERNET_STATUS_CONNECTION_CLOSED:
-            // Mark the connection as closed, but don't close the handle yet.
-            // This is because this may happen in at least two different
-            // situations:
-            // 1. Connection prematurely closed, we need to report an error.
-            // 2. Redirect to another site (WinInet closes connection, starts
-            //    another one), in which case we DON'T want to error out.
-            context->wasClosed = true;
-            break;
-
-        case INTERNET_STATUS_REDIRECT:
-            // Activity like this following INTERNET_STATUS_CONNECTION_CLOSED
-            // implies that the handle is still doing useful work, so unflag
-            // it. Otherwise we'd get an error at the very end of download.
-            //
-            // See https://github.com/vslavik/winsparkle/pull/139 for what this
-            // mess is about.
-            context->wasClosed = false;
             break;
     }
 }
@@ -349,9 +330,7 @@ void DownloadFile(const std::string& url, IDownloadSink *sink, Thread *onThread,
 
         if (ibuf.dwBufferLength == 0)
         {
-            // This check is required in case the INTERNET_STATUS_CONNECTION_CLOSED event was 
-            // received (and the handle was closed) during the call to InternetReadFileEx()
-            if (context.wasClosed)
+            if (context.lastError != ERROR_SUCCESS)
                 throw Win32Exception();
             else
                 break; // all of the file was downloaded