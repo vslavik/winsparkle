vslavik/winsparkle

commit cb4b0fd7c788bed0b2e94eb19832e05ab32a2916

Wait in background and trigger update on interval

src/dll_api.cpp
@@ -75,21 +75,8 @@ WIN_SPARKLE_API void __cdecl win_sparkle_init()
         {
             if ( checkUpdates )
             {
-                static const time_t ONE_DAY = 60*60*24;
-
-                time_t lastCheck = 0;
-                Settings::ReadConfigValue("LastCheckTime", lastCheck);
-                const time_t currentTime = time(NULL);
-
-                // Only check for updates in reasonable intervals:
-                const int interval = win_sparkle_get_update_check_interval();
-                if ( currentTime - lastCheck >= interval )
-                {
-                    // Run the check in background. Only show UI if updates
-                    // are available.
-                    UpdateChecker *check = new UpdateChecker();
-                    check->Start();
-                }
+                UpdateChecker *check = new UpdateChecker();
+                check->Start();
             }
         }
         else // not yet configured

src/updatechecker.cpp
@@ -36,6 +36,7 @@
 #include <vector>
 #include <cstdlib>
 #include <algorithm>
+#include <winsparkle.h>
 
 using namespace std;
 
@@ -222,6 +223,29 @@ void UpdateChecker::Run()
     // no initialization to do, so signal readiness immediately
     SignalReady();
 
+    while (1)
+    {
+        bool checkUpdates;
+        Settings::ReadConfigValue("CheckForUpdates", checkUpdates, false);
+        if (checkUpdates)
+        {
+            time_t lastCheck = 0;
+            Settings::ReadConfigValue("LastCheckTime", lastCheck);
+            const time_t currentTime = time(NULL);
+
+            // Only check for updates in reasonable intervals:
+            const int interval = win_sparkle_get_update_check_interval();
+            if (currentTime - lastCheck >= interval)
+            {
+                PerformUpdateCheck();
+            }
+        }
+        m_terminateEvent.WaitUntilSignaled(win_sparkle_get_update_check_interval());
+    }
+}
+
+void UpdateChecker::PerformUpdateCheck()
+{
     try
     {
         const std::string url = Settings::GetAppcastURL();
@@ -302,4 +326,11 @@ bool ManualUpdateChecker::ShouldSkipUpdate(const Appcast&) const
     return false;
 }
 
+void ManualUpdateChecker::Run()
+{
+    // no initialization to do, so signal readiness immediately
+    SignalReady();
+
+    PerformUpdateCheck();
+}
 } // namespace winsparkle

src/updatechecker.h
@@ -71,6 +71,7 @@ protected:
 
 protected:
     virtual void Run();
+    virtual void PerformUpdateCheck();
     virtual bool IsJoinable() const { return false; }
 };
 
@@ -87,6 +88,7 @@ public:
 protected:
     virtual int GetAppcastDownloadFlags() const;
     virtual bool ShouldSkipUpdate(const Appcast& appcast) const;
+    void Run();
 };
 
 

commit 55fb7b51fde7afa7fdbbc7aaf86087784f68d78b

Fix win_sparkle_check_update_without_ui to check immediately
Fixes regression introduced in cb4b0fd after which another periodic
checking thread would be launched by
win_sparkle_check_update_without_ui() and possibly not do any checking
(if it was done recently).

Split UpdateChecker into classes with explicitly declared purpose: one
for periodic checks, one for single-shot ones, and use the latter in
win_sparkle_check_update_without_ui().

Fixes #154.

src/dll_api.cpp
@@ -75,7 +75,7 @@ WIN_SPARKLE_API void __cdecl win_sparkle_init()
         {
             if ( checkUpdates )
             {
-                UpdateChecker *check = new UpdateChecker();
+                UpdateChecker *check = new PeriodicUpdateChecker();
                 check->Start();
             }
         }
@@ -342,7 +342,7 @@ WIN_SPARKLE_API void __cdecl win_sparkle_check_update_without_ui()
     {
         // Run the check in background. Only show UI if updates
         // are available.
-        UpdateChecker *check = new UpdateChecker();
+        UpdateChecker *check = new OneShotUpdateChecker();
         check->Start();
     }
     CATCH_ALL_EXCEPTIONS

src/ui.cpp
@@ -1390,7 +1390,8 @@ void App::OnAskForPermission(wxThreadEvent& event)
 
     if ( shouldCheck )
     {
-        UpdateChecker *check = new UpdateChecker();
+        // same as in win_sparkle_init()
+        UpdateChecker *check = new PeriodicUpdateChecker();
         check->Start();
     }
 }

src/updatechecker.cpp
@@ -218,44 +218,6 @@ UpdateChecker::UpdateChecker(): Thread("WinSparkle updates check")
 {
 }
 
-void UpdateChecker::Run()
-{
-    // no initialization to do, so signal readiness immediately
-    SignalReady();
-
-    while (true)
-    {
-        // time to wait for next iteration: either a reasonable default or
-        // time to next scheduled update check if checks are enabled
-        unsigned sleepTimeInSeconds = 60 * 60; // 1 hour
-
-        bool checkUpdates;
-        Settings::ReadConfigValue("CheckForUpdates", checkUpdates, false);
-
-        if (checkUpdates)
-        {
-            const time_t currentTime = time(NULL);
-            time_t lastCheck = 0;
-            Settings::ReadConfigValue("LastCheckTime", lastCheck);
-
-            // Only check for updates in reasonable intervals:
-            const int interval = win_sparkle_get_update_check_interval();
-            time_t nextCheck = lastCheck + interval;
-            if (currentTime >= nextCheck)
-            {
-                PerformUpdateCheck();
-                sleepTimeInSeconds = interval;
-            }
-            else
-            {
-                sleepTimeInSeconds = unsigned(nextCheck - currentTime);
-            }
-        }
-
-        m_terminateEvent.WaitUntilSignaled(sleepTimeInSeconds * 1000);
-    }
-}
-
 void UpdateChecker::PerformUpdateCheck()
 {
     try
@@ -317,6 +279,54 @@ bool UpdateChecker::ShouldSkipUpdate(const Appcast& appcast) const
 }
 
 
+void PeriodicUpdateChecker::Run()
+{
+    // no initialization to do, so signal readiness immediately
+    SignalReady();
+
+    while (true)
+    {
+        // time to wait for next iteration: either a reasonable default or
+        // time to next scheduled update check if checks are enabled
+        unsigned sleepTimeInSeconds = 60 * 60; // 1 hour
+
+        bool checkUpdates;
+        Settings::ReadConfigValue("CheckForUpdates", checkUpdates, false);
+
+        if (checkUpdates)
+        {
+            const time_t currentTime = time(NULL);
+            time_t lastCheck = 0;
+            Settings::ReadConfigValue("LastCheckTime", lastCheck);
+
+            // Only check for updates in reasonable intervals:
+            const int interval = win_sparkle_get_update_check_interval();
+            time_t nextCheck = lastCheck + interval;
+            if (currentTime >= nextCheck)
+            {
+                PerformUpdateCheck();
+                sleepTimeInSeconds = interval;
+            }
+            else
+            {
+                sleepTimeInSeconds = unsigned(nextCheck - currentTime);
+            }
+        }
+
+        m_terminateEvent.WaitUntilSignaled(sleepTimeInSeconds * 1000);
+    }
+}
+
+
+void OneShotUpdateChecker::Run()
+{
+    // no initialization to do, so signal readiness immediately
+    SignalReady();
+
+    PerformUpdateCheck();
+}
+
+
 /*--------------------------------------------------------------------------*
                             ManualUpdateChecker
  *--------------------------------------------------------------------------*/
@@ -330,11 +340,4 @@ bool ManualUpdateChecker::ShouldSkipUpdate(const Appcast&) const
     return false;
 }
 
-void ManualUpdateChecker::Run()
-{
-    // no initialization to do, so signal readiness immediately
-    SignalReady();
-
-    PerformUpdateCheck();
-}
 } // namespace winsparkle

src/updatechecker.h
@@ -67,24 +67,38 @@ protected:
     virtual bool ShouldAutomaticallyInstall() const { return false; }
 
 protected:
-    virtual void Run();
     virtual void PerformUpdateCheck();
     virtual bool IsJoinable() const { return false; }
+};
+
+
+/// Runs in the background and performs periodic update checks
+class PeriodicUpdateChecker : public UpdateChecker
+{
+protected:
+    virtual void Run();
+};
+
+
+/// Single-use checker: checks for updates and terminates itself
+class OneShotUpdateChecker : public UpdateChecker
+{
+protected:
+    virtual void Run();
 };
 
 
 /**
     Update checker used for manual checking.
  */
-class ManualUpdateChecker : public UpdateChecker
+class ManualUpdateChecker : public OneShotUpdateChecker
 {
 public:
     /// Creates checker thread.
-    ManualUpdateChecker() : UpdateChecker() {}
+    ManualUpdateChecker() : OneShotUpdateChecker() {}
 
 protected:
     virtual bool ShouldSkipUpdate(const Appcast& appcast) const;
-    void Run();
 };
 
 