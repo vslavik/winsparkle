5ab187660279c560c4d9ab6e57e4ae68217447f3
Václav Slavík <vaclav@slavik.io>
2021-03-02 19:34:44 +0100

    Update copyright years for 2021

--- src/updatechecker.cpp
+++ src/updatechecker.cpp
@@ -1,7 +1,7 @@
 /*
  *  This file is part of WinSparkle (https://winsparkle.org)
  *
- *  Copyright (C) 2009-2020 Vaclav Slavik
+ *  Copyright (C) 2009-2021 Vaclav Slavik
  *  Copyright (C) 2007 Andy Matuschak
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a
