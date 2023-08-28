vslavik/winsparkle

commit ec271892eaefd48edffd9ec2123fd9f9f09f5113

Open all release notes links in default browser
Intercept all navigation and open links present in release notes in
user's default browser instead of inline or in Edge (or worse, MSIE).

Fixes #177.

src/ui.cpp
@@ -1013,6 +1013,17 @@ void UpdateDialog::ShowReleaseNotes(const Appcast& info)
         auto sizer = new wxBoxSizer(wxVERTICAL);
         sizer->Add(m_webBrowser, wxSizerFlags(1).Expand());
         m_browserParent->SetSizer(sizer);
+
+        // Open all links in the default browser:
+		m_webBrowser->Bind(wxEVT_WEBVIEW_NAVIGATING, [info](wxWebViewEvent& evt)
+			{
+				auto url = evt.GetURL();
+                if (url.starts_with("http") && url != info.ReleaseNotesURL)
+                {
+                    wxLaunchDefaultBrowser(url);
+                    evt.Veto();
+                }
+			});
     }
 
     if( !info.ReleaseNotesURL.empty() )

commit a63e1d75db77f01e340ae9b01ca0789af32cb214

Fix blank/empty embedded release notes browser
Delay binding of opening external links in default browser
until after we've loaded the release notes
in the embedded browser.

Otherwise the release notes url is opened
in the external browser too,
which leaves the embedded browser blank/empty.

Fixes regression introduced in:
ec271892eaefd48edffd9ec2123fd9f9f09f5113

src/ui.cpp
@@ -1013,17 +1013,6 @@ void UpdateDialog::ShowReleaseNotes(const Appcast& info)
         auto sizer = new wxBoxSizer(wxVERTICAL);
         sizer->Add(m_webBrowser, wxSizerFlags(1).Expand());
         m_browserParent->SetSizer(sizer);
-
-        // Open all links in the default browser:
-        m_webBrowser->Bind(wxEVT_WEBVIEW_NAVIGATING, [info](wxWebViewEvent& evt)
-        {
-            auto url = evt.GetURL();
-            if (url.starts_with("http") && url != info.ReleaseNotesURL)
-            {
-                wxLaunchDefaultBrowser(url);
-                evt.Veto();
-            }
-        });
     }
 
     if( !info.ReleaseNotesURL.empty() )
@@ -1036,6 +1025,17 @@ void UpdateDialog::ShowReleaseNotes(const Appcast& info)
     }
 
     SetWindowStyleFlag(GetWindowStyleFlag() | wxRESIZE_BORDER);
+
+    // Open all links in the default browser:
+    m_webBrowser->Bind(wxEVT_WEBVIEW_NAVIGATING, [info](wxWebViewEvent& evt)
+    {
+        auto url = evt.GetURL();
+        if (url.starts_with("http") && url != info.ReleaseNotesURL)
+        {
+            wxLaunchDefaultBrowser(url);
+            evt.Veto();
+        }
+    });
 }
 
 