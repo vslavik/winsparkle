vslavik/winsparkle

commit ec271892eaefd48edffd9ec2123fd9f9f09f5113

Open all release notes links in default browser
Intercept all navigation and open links present in release notes in
user's default browser instead of inline or in Edge (or worse, MSIE).

Fixes #177.

src/ui.cpp
@@ -1013,6 +1013,17 @@ void UpdateDialog::ShowReleaseNotes(const Appcast& info)
         auto sizer = new wxBoxSizer(wxVERTICAL);
         sizer->Add(m_webBrowser, wxSizerFlags(1).Expand());
         m_browserParent->SetSizer(sizer);
+
+        // Open all links in the default browser:
+		m_webBrowser->Bind(wxEVT_WEBVIEW_NAVIGATING, [info](wxWebViewEvent& evt)
+			{
+				auto url = evt.GetURL();
+                if (url.starts_with("http") && url != info.ReleaseNotesURL)
+                {
+                    wxLaunchDefaultBrowser(url);
+                    evt.Veto();
+                }
+			});
     }
 
     if( !info.ReleaseNotesURL.empty() )

commit 6dc0d49d7890ae3c341eb224422391513c8d9012

Reformat inconsistent whitespace
Fix mixture of tabs vs spaces introduced in:
ec271892eaefd48edffd9ec2123fd9f9f09f5113

src/ui.cpp
@@ -1015,15 +1015,15 @@ void UpdateDialog::ShowReleaseNotes(const Appcast& info)
         m_browserParent->SetSizer(sizer);
 
         // Open all links in the default browser:
-		m_webBrowser->Bind(wxEVT_WEBVIEW_NAVIGATING, [info](wxWebViewEvent& evt)
-			{
-				auto url = evt.GetURL();
-                if (url.starts_with("http") && url != info.ReleaseNotesURL)
-                {
-                    wxLaunchDefaultBrowser(url);
-                    evt.Veto();
-                }
-			});
+        m_webBrowser->Bind(wxEVT_WEBVIEW_NAVIGATING, [info](wxWebViewEvent& evt)
+        {
+            auto url = evt.GetURL();
+            if (url.starts_with("http") && url != info.ReleaseNotesURL)
+            {
+                wxLaunchDefaultBrowser(url);
+                evt.Veto();
+            }
+        });
     }
 
     if( !info.ReleaseNotesURL.empty() )