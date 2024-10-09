3a8379295c7f94dd34d9969ee5c623827338f5c4
Ingo Randolf <ingo.randolf@servus.at>
2024-10-09 12:17:35 +0200

    correct closing of item node and check windows and update item to stop parsing

--- src/appcast.cpp
+++ src/appcast.cpp
@@ -300,12 +300,13 @@ void XMLCALL OnEndElement(void *data, const char *name)
         {
             ctxt.in_min_autoupdate_version--;
         }
-    }
-    else if (ctxt.in_channel && strcmp(name, NODE_ITEM) == 0)
-    {
-        ctxt.in_item--;
-        if (is_suitable_windows_item(ctxt.items[ctxt.items.size() - 1]))
-            XML_StopParser(ctxt.parser, XML_TRUE);
+        else if (strcmp(name, NODE_ITEM) == 0)
+        {
+            ctxt.in_item--;
+
+            if (is_suitable_windows_and_update_item(ctxt.items.back()))
+                XML_StopParser(ctxt.parser, XML_TRUE);
+        }
     }
     else if (strcmp(name, NODE_CHANNEL) == 0 )
     {
