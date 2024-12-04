vslavik/winsparkle

commit da4e7e2b5d9f6ca4209ee51fd3190bc06d46f6b1

Support major.minor.build versions in <sparkle:minimumSystemVersion>
Previously much less useful major.minor.servicepack was used, which was
of limited practical applicability and is completely useless since
Windows 10.

To preserve backwards compatiblity, treat low values in the 3rd value of
the tuple as service pack version.

src/appcast.cpp
@@ -54,12 +54,18 @@ namespace
 
 // Misc helper functions:
 
-bool is_compatible_with_windows_version(const Appcast &item)
+bool is_compatible_with_windows_version(const Appcast &item_)
 {
+    Appcast item(item_);
+
+    item.MinOSVersion = "10";
+
     if (item.MinOSVersion.empty())
         return true;
 
-    OSVERSIONINFOEXW osvi = { sizeof(osvi), 0, 0, 0, 0, { 0 }, 0, 0 };
+    OSVERSIONINFOEXW osvi = {};
+    osvi.dwOSVersionInfoSize = sizeof(osvi);
+
     DWORDLONG const dwlConditionMask = VerSetConditionMask(
         VerSetConditionMask(
         VerSetConditionMask(
@@ -67,8 +73,21 @@ bool is_compatible_with_windows_version(const Appcast &item)
         VER_MINORVERSION, VER_GREATER_EQUAL),
         VER_SERVICEPACKMAJOR, VER_GREATER_EQUAL);
 
-    sscanf(item.MinOSVersion.c_str(), "%lu.%lu.%hu", &osvi.dwMajorVersion,
-        &osvi.dwMinorVersion, &osvi.wServicePackMajor);
+    const int parsed = sscanf(item.MinOSVersion.c_str(), "%lu.%lu.%lu",
+                              &osvi.dwMajorVersion, &osvi.dwMinorVersion, &osvi.dwBuildNumber);
+    if (parsed == 0)
+    {
+        // failed to parse version number, ignore the value
+        return true;
+    }
+
+    // backwards compatibility with WinSparkle < 0.8.2 which used major.minor.sp
+	// instead of major.minor.build for the version number:
+    if (parsed == 3 && osvi.dwBuildNumber < 100)
+    {
+        osvi.wServicePackMajor = static_cast<WORD>(osvi.dwBuildNumber);
+		osvi.dwBuildNumber = 0;
+    }
 
     return VerifyVersionInfoW(&osvi, VER_MAJORVERSION | VER_MINORVERSION |
         VER_SERVICEPACKMAJOR, dwlConditionMask) != FALSE;

commit 134ab6d647f3a1c5a36dd2c73fdd25a6a735baf5

Remove accidentally commited debugging code
Fixes accidentally committed broken code in da4e7e2b.

Fixes #284.

src/appcast.cpp
@@ -54,12 +54,8 @@ namespace
 
 // Misc helper functions:
 
-bool is_compatible_with_windows_version(const Appcast &item_)
+bool is_compatible_with_windows_version(const Appcast &item)
 {
-    Appcast item(item_);
-
-    item.MinOSVersion = "10";
-
     if (item.MinOSVersion.empty())
         return true;
 