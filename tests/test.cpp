7debefa50c549701029ac2a0fa45a9f0fb73b0b4
Václav Slavík <vaclav@slavik.io>
2026-07-21 18:12:44 +0200

    Scaffolding for a test suite

---
+++ tests/test.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+
+namespace foobar {
+
+    TEST(TestCaseName, TestName) {
+        EXPECT_EQ(1, 1);
+
+        //char* xxx = (char*)malloc(10);
+        //char z = xxx[12];
+
+        EXPECT_TRUE(true);
+    }
+
+}
